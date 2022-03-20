38ea4d2fcd7db830bf6c3b0a08896ee056b2223f
Umberto Breglia <bregliascuola2002@gmail.com>
2022-03-20 13:09:36 +0100

    fixed input validation

--- inputValidation.h
+++ inputValidation.h
@@ -5,9 +5,8 @@
 #include <limits>
 
 
-
 template<typename T>
-void getInput(T& input, const std::string msg)
+void getInput(T& input, const std::string msg) 
 {
     std::cout << msg << std::endl;
     std::cin >> input;
@@ -23,22 +22,28 @@ void getInput(T& input, const std::string msg)
     }
 }
 
-// number with spaces
-/*void getInput(std::string& input, const std::string msg)
+bool isNumber(const std::string& str)
 {
-    std::cout << msg << std::endl;
-    std::cin.ignore();
-    std::getline(std::cin,input);
-
-
-    while (std::cin.fail())  // Input validation using limits library
-    {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Input error,please try again\n";
-
-        std::cout << msg << std::endl;  
-        std::getline(std::cin,input);
+    for (char const &c : str) {
+        if (std::isdigit(c) == 0) return false;
     }
-   
-}*/
+    return true;
+}
+
+// number with spaces
+void getInput(std::string& input, const std::string msg)
+{
+    do
+    {
+        std::cout << msg << std::endl;  
+        std::cin >> input;
+
+        //std::cin.ignore();
+        // std::getline(std::cin,input);
+
+        if(!isNumber(input))
+        {
+            std::cout << "Input error,please try again\n";
+        }
+    } while (!isNumber(input));
+}
